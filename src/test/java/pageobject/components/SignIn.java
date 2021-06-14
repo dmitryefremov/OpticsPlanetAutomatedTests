51eed05f883c7f27fdfefe008ed5a468a71d0e85
Dmitrijs Jefremovs <dmitrijs.jefremovs@gmail.com>
2021-06-14 18:20:35 +0300

    Updates

--- src/test/java/pageobject/components/SignIn.java
+++ src/test/java/pageobject/components/SignIn.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import pageobject.pages.BaseFunc;
 import pageobject.pages.ProductGridPage;
 
 public class SignIn {
@@ -22,48 +23,32 @@ public class SignIn {
 
     private final Logger LOGGER = LogManager.getLogger(ProductGridPage.class);
 
+    private BaseFunc baseFunc = new BaseFunc();
+
     private WebDriverWait wait;
     private WebDriver driver;
 
     //Use this String for prefilling your e-mail
-    String signInEmail = "your@email.com;
+    String signInEmail = "your@email.com";
     //Use this String for prefilling your password
-    String signInPassword = "zzzz";
-
-    @BeforeEach
-    public void beforeEach() {
-        try {
-            String chromeDriverPath = System.getenv("chrome_driver_path");
-            System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-        } catch (NullPointerException e) {
-            System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
-        }
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless",
-                "--disable-gpu", "--window-size=1920,1200", "--ignore-certificate-errors",
-                "--disable-extensions", "--no-sandbox", "--disable-dev-shm-usage");
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        LOGGER.info("Opening OpticsPlanet Home Page");
-        driver.get("https://www.opticsplanet.com/");
-        wait = new WebDriverWait(driver, 10);
-    }
+    String signInPassword = "password";
 
     @Test
     public void signIn() { //Signing in as an existing customer
+        baseFunc.openPage("https://www.opticsplanet.com/");
         LOGGER.info("Clicking Sign In Button");
-        wait.until(ExpectedConditions.elementToBeClickable(SIGN_IN_BUTTON)).click();
+        baseFunc.click(SIGN_IN_BUTTON);
         LOGGER.info("Providing customer e-mail and password");
-        wait.until(ExpectedConditions.elementToBeClickable(SIGN_IN_EMAIL_INPUT)).sendKeys(signInEmail);
-        wait.until(ExpectedConditions.elementToBeClickable(SIGN_IN_PASSWORD_INPUT)).sendKeys(signInPassword);
+        baseFunc.type(SIGN_IN_EMAIL_INPUT, signInEmail);
+        baseFunc.type(SIGN_IN_PASSWORD_INPUT, signInPassword);
         LOGGER.info("Trying to Sign In");
-        wait.until(ExpectedConditions.elementToBeClickable(SIGN_IN_SUBMIT_BTN)).click();
+        baseFunc.click(SIGN_IN_SUBMIT_BTN);
         LOGGER.info("Checking customer is really signed in");
-        wait.until(ExpectedConditions.elementToBeClickable(CUSTOMER_AUTHORIZED_PROOF));
+        baseFunc.isVisible(CUSTOMER_AUTHORIZED_PROOF);
     }
 
     @AfterEach
     public void closeBrowser() {
-        driver.close();
+        baseFunc.closeBrowser();
     }
 }
